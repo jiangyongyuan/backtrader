9c7c3b5b16debfbb6f630fa713b0bd5487b2739e
jiang.yy@qq.com <jiang.yy@qq.com>
2020-09-28 09:21:55 +0800

    add hello world

--- demo/hello_world.py
+++ demo/hello_world.py
@@ -43,6 +43,30 @@ class TestStrategy(bt.Strategy):
         bt.indicators.SmoothedMovingAverage(rsi, period=10)
         bt.indicators.ATR(self.datas[0], plot=False)
 
+        # 获取股票的收盘价
+
+    # close_data = get_bars(security, count=5, unit='1d', fields=['close'])
+    # # 取得过去五天的平均价格
+    # MA5 = close_data['close'].mean()
+    # # 取得上一时间点价格
+    # current_price = close_data['close'][-1]
+    # # 取得当前的现金
+    # cash = context.portfolio.available_cash
+    #
+    # # 如果上一时间点价格高出五天平均价1%, 则全仓买入
+    # if (current_price > 1.01 * MA5) and (cash > 0):
+    #     # 记录这次买入
+    #     log.info("价格高于均价 1%%, 买入 %s" % (security))
+    #     print("当前可用资金为{0}, position_value为{0}".format(cash, context.portfolio.positions_value))
+    #     # 用所有 cash 买入股票
+    #     order_value(security, cash)
+    # # 如果上一时间点价格低于五天平均价, 则空仓卖出
+    # elif current_price < MA5 and context.portfolio.positions[security].closeable_amount > 0:
+    #     # 记录这次卖出
+    #     log.info("价格低于均价, 卖出 %s" % (security))
+    #     # 卖出所有股票,使这只股票的最终持有量为0
+    #     order_target(security, 0)
+
 
 
 
@@ -115,14 +139,6 @@ if __name__ == '__main__':
         todate=datetime.datetime(2019, 12, 31),
         reverse=False
     )
-
-    # ('datetime', 0),
-    # ('time', -1),
-    # ('open', 1),
-    # ('high', 2),
-    # ('low', 3),
-    # ('close', 4),
-    # ('volume', 5),
 
     # Add the Data Feed to Cerebro
     cerebro.adddata(data)
